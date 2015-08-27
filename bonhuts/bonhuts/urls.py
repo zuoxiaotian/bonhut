a28f74e766e47fa621a52f1831bb90afb366cd2c
zuoxiaotian <mr.xiaotian.zuo@gmail.com>
2015-08-27 17:24:45 +0200

    necessary package + header + footer

--- bonhuts/bonhuts/urls.py
+++ bonhuts/bonhuts/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
+from exchange.views             import views
+
 admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testdjango.views.home', name='home'),
     #url(r'^blog/', include('blog.urls')),
+    url(r'^$'       , views.default         , name = 'default'          )   ,  # @UndefinedVariable
+                       
     url(r'^admin/', include(admin.site.urls)),
     url(r'^exchange/', include('exchange.urls'))
 )
--- bonhuts/exchange/views/views_login_logout.py
+++ bonhuts/exchange/views/views_login_logout.py
@@ -1,7 +1,6 @@
 # Create your views here.
 
 
-@login_required  # @UndefinedVariable
 def default(request):
     test = {"st": "test st"}
     return render(request, "../templates/default.html", test)  # @UndefinedVariable
